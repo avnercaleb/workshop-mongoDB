0413329f71171d3da34b007b7ae443bb9991cb09
avnercaleb <avnercaleb@hotmail.com>
2022-11-08 20:29:18 -0300

    Criação dos endpoints POST e DELETE

--- src/main/java/com/workshopmongo/controller/UserController.java
+++ src/main/java/com/workshopmongo/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.workshopmongo.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.workshopmongo.entities.User;
 import com.workshopmongo.entities.dto.UserDTO;
@@ -32,5 +37,20 @@ public class UserController {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO){
+		User user = service.fromDTO(userDTO);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/workshopmongo/services/UserService.java
+++ src/main/java/com/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshopmongo.entities.User;
+import com.workshopmongo.entities.dto.UserDTO;
 import com.workshopmongo.repositories.UserRepository;
 import com.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -20,9 +21,21 @@ public class UserService {
 		return repository.findAll();
 	}
 	
-	public User findById(String id) {
-		
+	public User findById(String id) {		
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
+	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
